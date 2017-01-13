2b60bc35f98ad4b8b7df56e0a7a7502720861ea4
Gordon Smith <gordonjsmith@gmail.com>
2017-01-13 11:23:30 +0000

    CHeck travis env
    Signed-off-by: Gordon Smith <gordonjsmith@gmail.com>

--- test/ESPConnection.ts
+++ test/ESPConnection.ts
@@ -1,10 +1,12 @@
 import { expect } from "chai";
 import { ESPConnection } from "../src/comms"
 
-const PUBLIC_URL = `http://192.168.3.22:8010/WsWorkunits`;
+declare var process: any;
+
+const VM_URL = "http://192.168.3.22:8010/WsWorkunits";
+const PUBLIC_URL = "http://52.51.90.23:8010/WsWorkunits";
 
 describe("ESPConnection", function () {
-    this.timeout(10000);
     it("basic-post", function () {
         var espConnection = new ESPConnection(PUBLIC_URL);
         expect(espConnection).to.be.not.null;
@@ -42,37 +44,39 @@ describe("ESPConnection", function () {
     });
 });
 
-describe.skip("ESPConnection-dataland", function () {
-    it("basic", function () {
-        var espConnection = new ESPConnection("http://10.241.12.207:8010/WsWorkunits");
-        expect(espConnection).to.be.not.null;
-        espConnection.userID = "gosmith";
-        espConnection.userPW = "???";
-        return espConnection.post("WUQuery", { PageSize: 2 }).then((response) => {
-            expect(response).to.be.not.undefined;
-            expect(response.hasContent()).to.be.true;
-            return response;
+if (!process.env.TRAVIS) {
+    describe.skip("ESPConnection-dataland", function () {
+        it("basic", function () {
+            var espConnection = new ESPConnection("http://10.241.12.207:8010/WsWorkunits");
+            expect(espConnection).to.be.not.null;
+            espConnection.userID = "gosmith";
+            espConnection.userPW = "???";
+            return espConnection.post("WUQuery", { PageSize: 2 }).then((response) => {
+                expect(response).to.be.not.undefined;
+                expect(response.hasContent()).to.be.true;
+                return response;
+            });
         });
     });
-});
 
-describe("ESPConnection-vm", function () {
-    this.timeout(10000);
-    it("basic", function () {
-        var espConnection = new ESPConnection(PUBLIC_URL);
-        expect(espConnection).to.be.not.null;
-        return espConnection.post("WUQuery", { PageSize: 2 }).then((response) => {
-            expect(response).to.be.not.undefined;
-            expect(response.__exceptions).to.be.undefined;
+    describe("ESPConnection-vm", function () {
+        it("basic", function () {
+            var espConnection = new ESPConnection(VM_URL);
+            expect(espConnection).to.be.not.null;
+            return espConnection.post("WUQuery", { PageSize: 2 }).then((response) => {
+                expect(response).to.be.not.undefined;
+                expect(response.__exceptions).to.be.undefined;
+            });
+        });
+
+        it("exception", function () {
+            var espConnection = new ESPConnection(VM_URL);
+            expect(espConnection).to.be.not.null;
+            return espConnection.post("WUInfo", { MissingWUID: "" }).then((response) => {
+                expect(response).to.be.not.undefined;
+                expect(response.__exceptions).to.be.not.undefined;
+            });
         });
     });
-    it("exception", function () {
-        var espConnection = new ESPConnection(PUBLIC_URL);
-        expect(espConnection).to.be.not.null;
-        return espConnection.post("WUInfo", { MissingWUID: "" }).then((response) => {
-            expect(response).to.be.not.undefined;
-            expect(response.__exceptions).to.be.not.undefined;
-        });
-    });
-});
+}
 
