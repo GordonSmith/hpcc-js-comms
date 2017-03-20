d32559cbcc8cfd3d0a4e054148a4730e5a2ecac7
Richard Chapman <richardkchapman@gmail.com>
2017-03-20 11:17:55 +0000

    WIP
    Signed-off-by: Gordon Smith <gordonjsmith@gmail.com>

--- src/index-common.ts
+++ src/index-common.ts
@@ -1,3 +1,26 @@
 export const version = "0.0.1";
 
+//  Util/Collections  - Should be its own package?
+export { logger, scopedLogger, Level } from "./util/logging";
+export { IObserverHandle } from "./util/observer";
+export { espTime2Seconds } from "./util/esp";
+export { Graph, IGraphItem } from "./collections/graph";
+
+//  Comms transport - Should be its own package?
 export { Connection, RequestType, ResponseType } from "./comms/connection";
+
+//  Raw Services  ---
+export { Service as WsWorkunits, WUAction } from "./esp/services/wsWorkunits";
+export { Service as WsTopology } from "./esp/services/wsTopology";
+export { Service as WsSMC } from "./esp/services/wsSMC";
+export { Service as WsDFU } from "./esp/services/wsDFU";
+
+//  OO Wrappers  ---
+export { ECLGraph } from "./esp/ecl/graph";
+export { Resource } from "./esp/ecl/resource";
+export { Result } from "./esp/ecl/result";
+export { SourceFile } from "./esp/ecl/sourceFile";
+export { Timer } from "./esp/ecl/timer";
+export { Workunit } from "./esp/ecl/workunit";
+
+// export { Test } from "./test";
---
+++ test/demo.ts
@@ -0,0 +1,64 @@
+import { Workunit } from "../src/esp/ecl/workunit";
+import { scopedLogger } from "../src/util/logging";
+
+const logger = scopedLogger(`${__dirname}/${__filename}`);
+
+describe.skip("Readme quick start", function () {
+    it("Quick Submit", function () {
+        return Workunit.submit({ baseUrl: "http://192.168.3.22:8010", userID: "userID", password: "pw" }, "hthor", "'Hello and Welcome!';").then((wu) => {
+            return wu.watchUntilComplete();
+        }).then((wu) => {
+            return wu.fetchResults().then((results) => {
+                return results[0].fetchRows();
+            }).then((rows) => {
+                logger.debug(rows);
+                return wu;
+            });
+        }).then((wu) => {
+            return wu.delete();
+        }).catch((_e) => {
+            //  Recover
+        });
+    });
+
+    it("Quick Submit", function () {
+        return Workunit.submit({ baseUrl: "http://192.168.3.22:8010", userID: "userID", password: "pw" }, "hthor", "'Hello and Welcome!';").then((wu) => {
+            return wu.watchUntilComplete();
+        }).then((wu) => {
+            return wu.fetchResults().then((results) => {
+                return results[0].fetchRows();
+            }).then((rows) => {
+                logger.debug(rows);
+                return wu;
+            });
+        }).then((wu) => {
+            return wu.delete();
+        });
+    });
+
+    it("query", function () {
+        return Workunit.query({ baseUrl: "http://192.168.3.22:8010", userID: "userID", password: "pw" },
+            { State: "completed", LastNDays: 7, Count: 99 }).then((wus) => {
+                wus.forEach((wu) => {
+                    logger.debug(`${wu.Wuid} Total Cluster Time:  ${wu.TotalClusterTime}`);
+                });
+            });
+    });
+
+    it("resubmit", function () {
+        const eclWorkunit = Workunit.attach({ baseUrl: "http://192.168.3.22:8010", userID: "userID", password: "pw" }, "W20170404-085158xxx");
+        return eclWorkunit.resubmit()
+            .then((wu) => {
+                return wu.watchUntilComplete()
+                    .then(() => {
+                        return wu.fetchResults().then((results) => {
+                            return results[0].fetchRows();
+                        }).then((rows) => {
+                            logger.debug(rows);
+                        });
+                    });
+            }).catch((e) => {
+                logger.debug(e);
+            });
+    });
+});
