d0c824230784c84bb926f26cd5d68351849b7ae2
Gordon Smith <gordonjsmith@gmail.com>
2017-04-15 11:37:54 +0100

    Add client tools support to comms.
    Signed-off-by: Gordon Smith <gordonjsmith@gmail.com>

---
+++ test/clienttools/eclcc.ts
@@ -0,0 +1,46 @@
+import { expect } from "chai";
+
+import { ClientTools, locateClientTools } from "../../src/clienttools/eclcc";
+import { logger } from "../../src/util/Logging";
+import { ESP_URL, isTravis } from "../testLib";
+
+describe.only("eclcc", function () {
+    logger.debug(process.cwd());
+    let ct: ClientTools;
+    it("locateClientTools", function () {
+        return locateClientTools().then((clientTools) => {
+            expect(clientTools).to.exist;
+            logger.debug(clientTools.eclccPath);
+            ct = clientTools;
+            return clientTools;
+        });
+    });
+    it("createArchive", function () {
+        expect(ct).to.exist;
+        return ct.createArchive("./test/clienttools/test.ecl").then(archive => {
+            expect(archive).to.exist;
+            expect(archive.content).to.be.not.empty;
+            logger.debug(archive.content);
+            return archive;
+        });
+    });
+    it("syntaxCheck", function () {
+        expect(ct).to.exist;
+        return ct.syntaxCheck("./test/clienttools/syntaxErrors.ecl").then(errors => {
+            expect(errors).to.exist;
+            logger.debug(errors);
+            return errors;
+        });
+    });
+    it("createArchiveFail", function () {
+        expect(ct).to.exist;
+        return ct.createArchive("./test/clienttools/doesNotExist.ecl").then(archive => {
+            expect(archive).to.exist;
+            expect(archive.content).to.be.empty;
+            expect(archive.err).to.be.not.empty;
+            expect(archive.err.length).to.be.greaterThan(0);
+            logger.debug(archive.err);
+            return archive;
+        });
+    });
+});
