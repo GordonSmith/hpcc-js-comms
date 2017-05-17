a4597ef8c928c805a49e11eeae704153931dc558
Gordon Smith <gordonjsmith@gmail.com>
2017-05-17 06:32:05 +0100

    Rename test files to match src
    Signed-off-by: Gordon Smith <gordonjsmith@gmail.com>
    
    # Conflicts:
    #	test/esp/ecl/workunit.ts
    #	test/esp/services/wsWorkunits.ts

--- test/esp/ecl/Resource.ts
+++
@@ -1,9 +0,0 @@
-import { expect } from "chai";
-
-import { Resource } from "../../../src/esp/ecl/resource";
-
-describe("Resource", function () {
-    it("basic", function () {
-        expect(Resource).to.be.a("function");
-    });
-});
--- test/esp/ecl/Result.ts
+++
@@ -1,9 +0,0 @@
-import { expect } from "chai";
-
-import { Result } from "../../../src/esp/ecl/result";
-
-describe("Result", function () {
-    it("basic", function () {
-        expect(Result).to.be.a("function");
-    });
-});
