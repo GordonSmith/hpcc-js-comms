1a410b9fdc27f9c62a1812deacfbe7a947e56feb
Gordon Smith <gordonjsmith@gmail.com>
2017-05-17 06:32:05 +0100

    Rename test files to match src
    Signed-off-by: Gordon Smith <gordonjsmith@gmail.com>

--- test/esp/ecl/SourceFile.ts
+++
@@ -1,9 +0,0 @@
-import { expect } from "chai";
-
-import { SourceFile } from "../../../src/esp/ecl/sourceFile";
-
-describe("SourceFile", function () {
-    it("basic", function () {
-        expect(SourceFile).to.be.a("function");
-    });
-});
--- test/esp/ecl/Timer.ts
+++
@@ -1,9 +0,0 @@
-import { expect } from "chai";
-
-import { Timer } from "../../../src/esp/ecl/timer";
-
-describe("Timer", function () {
-    it("basic", function () {
-        expect(Timer).to.be.a("function");
-    });
-});
