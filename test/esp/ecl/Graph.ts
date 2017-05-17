a56812216d5f9e0b9d42bcb8b91c3933abef9b94
Gordon Smith <gordonjsmith@gmail.com>
2017-05-17 06:32:05 +0100

    Rename test files to match src
    Signed-off-by: Gordon Smith <gordonjsmith@gmail.com>

--- test/esp/ecl/Graph.ts
+++
@@ -1,9 +0,0 @@
-import { expect } from "chai";
-
-import { ECLGraph } from "../../../src/esp/ecl/graph";
-
-describe("Graph", function () {
-    it("basic", function () {
-        expect(ECLGraph).to.be.a("function");
-    });
-});
